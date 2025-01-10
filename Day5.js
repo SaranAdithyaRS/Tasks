b36b66917f2fde56ba0f2e68e8fe5d11892787fc
Saran Adithya <saranadithya2407@gmail.com>
2025-01-10 15:48:29 +0530

    Add files via upload

---
+++ Day5.js
@@ -0,0 +1,19 @@
+// let a=10;
+// console.log(a===10);
+// let v=3;
+// let c=5;
+// let b=v+a+c;
+// console.log(b);
+
+// if(b>=18){
+//     console.log("Eligible");
+// }else {
+//     console.log("Not Eligible");
+// }
+
+let a=5;
+if(a%2==0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
